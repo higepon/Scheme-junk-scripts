52b6eaa7dfe9cdd96a1aae59c40080e46ef6ffa9
Higepon (MINOWA Taro) <higepon@gmail.com>
2010-07-13 16:02:46 +0900

    Modified to limit number of processes.

--- store-key-value-from-file.scm
+++ store-key-value-from-file.scm
@@ -14,55 +14,52 @@
 (define *unit-size* 500)
 (define *hosts* '#("10.12.4.40" "10.12.4.41" "10.12.4.42" "10.12.4.43" "10.12.4.44"
                    "10.12.4.45" "10.12.4.46" "10.12.4.47" "10.12.4.48" "10.12.4.49"))
-(define host-index 0)
-(define (start-worker line*)
-  (let1 host (vector-ref *hosts* (mod host-index (vector-length *hosts*)))
-    (set! host-index (+ host-index 1))
-    (spawn (^(x)
-             (match x
-               [(l* host)
-                (let1 mc (memcached-connect host "11211")
-                  (for-each
-                   (^l
-                    (memcached-set! mc (car l) 0 "10" (cdr l)))
-                   l*)
-                  (memcached-close mc))
-                ]))
-           (list line* host)
-           '((rnrs) (mosh concurrent) (memcached) (mosh control) (shorten) (match) (mosh)))))
 
-(let ([lines (map (^x (read (open-string-input-port x))) (file->list *data-file*))])
+
+(define (start-worker host* line*)
+  (spawn (^(line*+host*)
+           ;; line* is list of (key . value)
+           (define (insert! host port line* num-line*-insert)
+             (let1 mc (memcached-connect host port)
+               (do ([i 0 (+ i 1)]
+                    [line* line* (cdr line*)])
+                   [(or (= i num-line*-insert) (null? line*))
+                    (memcached-close mc)
+                    line*]
+                 (memcached-set! mc (car (car line*)) 0 "10" (cdr (car line*))))))
+
+           (define (insert-all line* host* unit-size)
+             (random-source-randomize! default-random-source)
+             (let loop ([i (random-integer 1000)]
+                        [j 0]
+                        [line* line*])
+               (cond
+                [(null? line*) '()]
+                [else
+                 (let1 host (vector-ref host* (mod i (vector-length host*)))
+                   (format #t "~a:~d\n" host (* j unit-size))
+                   (let1 rest (insert! host "11211" line* unit-size)
+                     (loop (+ i 1) (+ j 1) rest)))])))
+
+           (match line*+host*
+             [(line* host*)
+              (insert-all line* host* 500)
+              ]))
+         (list line* host*)
+         '((srfi :27) (rnrs) (mosh concurrent) (memcached) (mosh control) (shorten) (match) (mosh))))
+
+
+(let* ([lines (map (^x (read (open-string-input-port x))) (file->list *data-file*))]
+       [lines-count-per-process (div (length lines) *number-of-processes*)])
   (let loop ([line* lines]
-             [pid* '()]
-             [i 0])
+             [pid* '()])
     (cond
-     [(= (length pid*) *number-of-processes*)
-      (join! (car (drop pid* (- (length pid*) 1))))
-      (format #t "~d/~d\n" (* (+ i 1) *unit-size*) (length lines))
-      (loop line* (take pid* (- (length pid*) 1)) (+ i 1))]
+     [(null? line*)
+      (for-each join! pid*)
+      (display "done")
+      (newline)]
      [else
-      (let-values (([head tail] (if (< (length line*) *unit-size*) (values line* '()) (split-at! line* *unit-size*))))
-        (let1 pid (start-worker head)
-          (loop tail (cons pid pid*) i)))])))
+      (let-values (([head tail] (if (< (length line*) lines-count-per-process) (values line* '()) (split-at! line* lines-count-per-process))))
+        (let1 pid (start-worker *hosts* head)
+          (loop tail (cons pid pid*))))])))
 
-
-;;     (let-values (([head tail] (if (< (length line*) *unit-size*) (values line* '()) (split-at! line* *unit-size*))))
-;;       (cond
-;;        [(null? head)
-;;         (display "join start!\n")
-;;         (for-each (^p (join! p)) pid*)
-;;         (display "done\n")]
-;;        [else
-;;           (let1 pid (spawn (^x
-;;                             (match x
-;;                               [(my-index line*)
-;;                                 (let1 mc (memcached-connect (vector-ref '#("10.12.4.40" "10.12.4.41") #;'#("10.12.0.2" "10.12.0.3" "10.12.0.16" "10.12.0.20") (mod my-index 4)) "11211")
-;;                                   (format #t "<~d> started\n" my-index)
-;;                                   (for-each
-;;                                    (^l
-;;                                     (memcached-set! mc (car l) 0 "10" (cdr l)))
-;;                                    line*)
-;;                                   (format #t "<~d> end\n" my-index))]))
-;;                            (list i line*)
-;;                            '((rnrs) (mosh concurrent) (memcached) (mosh control) (shorten) (match) (mosh)))
-;;             (loop tail (+ i 1) (cons pid pid*)))]))))
